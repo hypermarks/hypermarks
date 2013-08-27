a99fe8b729af2006fb87fbd414f1bea06273756a
Jehan Tremback <jehan.tremback@gmail.com>
2013-08-26 21:22:00 -0700

    Get rid of obsolete app.configure

--- config/express.js
+++ config/express.js
@@ -3,12 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express')
-var mongoStore = require('connect-mongo')(express)
-var helpers = require('view-helpers')
-var pkg = require('../package')
-var flash = require('connect-flash')
-var env = process.env.NODE_ENV || 'development'
+var express = require('express');
+var mongoStore = require('connect-mongo')(express);
+var helpers = require('view-helpers');
+var pkg = require('../package');
+var flash = require('connect-flash');
+var env = process.env.NODE_ENV || 'development';
 
 /*!
  * Expose
@@ -41,56 +41,54 @@ module.exports = function (app, config, passport) {
   app.set('views', config.root + '/app/views')
   app.set('view engine', 'jade')
 
-  app.configure(function () {
-    // bodyParser should be above methodOverride
-    app.use(express.bodyParser())
-    app.use(express.methodOverride())
+  // bodyParser should be above methodOverride
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
 
-    // cookieParser should be above session
-    app.use(express.cookieParser())
-    app.use(express.session({
-      secret: pkg.name,
-      store: new mongoStore({
-        url: config.db,
-        collection : 'sessions'
-      })
-    }))
-
-    // Passport session
-    app.use(passport.initialize())
-    app.use(passport.session())
-
-    // Flash messages
-    app.use(flash())
-
-    // expose pkg and node env to views
-    app.use(function (req, res, next) {
-      res.locals.pkg = pkg
-      res.locals.env = env
-      next()
+  // cookieParser should be above session
+  app.use(express.cookieParser())
+  app.use(express.session({
+    secret: pkg.name,
+    store: new mongoStore({
+      url: config.db,
+      collection : 'sessions'
     })
+  }))
 
-    // View helpers
-    app.use(helpers(pkg.name))
+  // Passport session
+  app.use(passport.initialize())
+  app.use(passport.session())
 
-    // routes should be at the last
-    app.use(app.router)
+  // Flash messages
+  app.use(flash())
 
-    // custom error handler
-    app.use(function (err, req, res, next) {
-      if (err.message
-        && (~err.message.indexOf('not found')
-        || (~err.message.indexOf('Cast to ObjectId failed')))) {
-        return next()
-      }
+  // expose pkg and node env to views
+  app.use(function (req, res, next) {
+    res.locals.pkg = pkg
+    res.locals.env = env
+    next()
+  })
 
-      console.error(err.stack)
-      res.status(500).render('500')
-    })
+  // View helpers
+  app.use(helpers(pkg.name))
 
-    app.use(function (req, res, next) {
-      res.status(404).render('404', { url: req.originalUrl })
-    })
+  // routes should be at the last
+  app.use(app.router)
+
+  // custom error handler
+  app.use(function (err, req, res, next) {
+    if (err.message
+      && (~err.message.indexOf('not found')
+      || (~err.message.indexOf('Cast to ObjectId failed')))) {
+      return next()
+    }
+
+    console.error(err.stack)
+    res.status(500).render('500')
+  })
+
+  app.use(function (req, res, next) {
+    res.status(404).render('404', { url: req.originalUrl })
   })
 
   // development specific stuff
