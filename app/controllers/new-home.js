125b4b080f67a39af83a2f7c2a8c1222c3b1f676
Jehan <jehan.tremback@gmail.com>
2013-09-09 10:24:26 -0700

    start on templates for home

--- app/controllers/new-home.js
+++ app/controllers/new-home.js
@@ -1,28 +1,32 @@
 module.exports = function(req, res) {
 		var results = [
 		{
-			"favicon": "false"
-			, "date_added": ""
-			, "title": "Da Mystery of Chessboxing"
-			, "link": "#"
+			'favicon': 'false'
+			, 'date_added': ''
+			, 'title': 'Da Mystery of Chessboxing'
+			, 'url': '#'
+			, 'sani_url': 'wutang.com/discography/da-mystery-of-chessboxin' 
 		}
 		, {
-			"favicon": "http://roots.cx/favicon.ico"
-			, "date_added": ""
-			, "title": "Weird Times in the Gold Mines"
-			, "link": "#"
+			'favicon': 'http://roots.cx/favicon.ico'
+			, 'date_added': ''
+			, 'title': 'Weird Times in the Gold Mines'
+			, 'url': '#'
+			, 'sani_url': 'doors.com/albums/wierd-times.html'
 		}
 		, {
-			"favicon": "https://github.com/favicon.ico"
-			, "date_added": ""
-			, "title": "Github - Shell script linter"
-			, "link": "#"
+			'favicon': 'https://github.com/favicon.ico'
+			, 'date_added': ''
+			, 'title': 'Github - Shell script linter'
+			, 'url': '#'
+			, 'sani_url': 'github.com/staylor/UI-Toolbox'
 		}
 		, {
-			"favicon": "false"
-			, "date_added": ""
-			, "title": "Ufo Research"
-			, "link": "#"
+			'favicon': 'false'
+			, 'date_added': ''
+			, 'title': 'Ufo Research'
+			, 'url': '#'
+			, 'sani_url': 'roswellufomuseum.com/research.html'
 		}
 	];
 
