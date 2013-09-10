d0e73d247c75d864a97a1c4e4b080c1ca40cbcd5
Jehan <jehan.tremback@gmail.com>
2013-09-10 04:31:53 -0700

    bookmarklet logs in from many sites, posting urls to /api/bookmarks puts them in the db

--- external/bookmarklet.js
+++ external/bookmarklet.js
@@ -1,18 +1,17 @@
 'use strict';
 
-!function () {
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'http://localhost:1337/api/bookmarks', true); //TODO: dynamically substitute urls for dev, prod, etc.
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+// !function () {
+// 	var xhr = new XMLHttpRequest();
+// 	xhr.open('POST', 'http://localhost:1337/api/bookmarks', true); //TODO: dynamically substitute urls for dev, prod, etc.
+// 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 	
-	xhr.addEventListener('error', function(e) {
-		console.log(xhr.status);
-	}, false);
+// 	xhr.addEventListener('error', function(e) {
+// 		console.log(xhr.status);
+// 	}, false);
 	
-	xhr.send('url=' + window.location);
-}();
+// 	xhr.send('url=' + window.location);
+// }();
 
-// //Testing external login
-// var redirectUrl = window.location;
-// alert(redirectUrl);
-// window.location.href = 'http://localhost:1337/auth/externalLogin?redirectUrl=' + redirectUrl;
+//Testing external login
+var redirectUrl = window.location;
+window.location.href = 'http://localhost:1337/auth/externalLogin?redirectUrl=' + redirectUrl;
