e0e44e7f8b418382ffd501a20184c0dcfb63bb68
Jehan <jehan.tremback@gmail.com>
2013-09-10 04:27:26 -0700

    getting bookmarklet to respond to not being logged in

--- external/bookmarklet.js
+++ external/bookmarklet.js
@@ -1,7 +1,18 @@
 'use strict';
 
+!function () {
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'http://localhost:1337/api/bookmarks', true); //TODO: dynamically substitute urls for dev, prod, etc.
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	
+	xhr.addEventListener('error', function(e) {
+		console.log(xhr.status);
+	}, false);
+	
+	xhr.send('url=' + window.location);
+}();
 
-//Testing external login
-var redirectUrl = window.location;
-alert(redirectUrl);
-window.location.href = 'http://localhost:1337/auth/externalLogin?redirectUrl=' + redirectUrl;
+// //Testing external login
+// var redirectUrl = window.location;
+// alert(redirectUrl);
+// window.location.href = 'http://localhost:1337/auth/externalLogin?redirectUrl=' + redirectUrl;
