6201533fb4630cb7e4001c210054371cc826b804
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-09-29 19:22:53 -0700

    getting back to home.

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,72 +1,78 @@
 'use strict';
 
 var mongoose = require('mongoose')
-  , Bookmark = mongoose.model('Bookmark')
-  , Address = mongoose.model('Address')
-  , stringUtils = require('../../utils/string-utils.js')
-  , _ = require('lodash');
+  , Bookmark = mongoose.model('Bookmark')
+  , Address = mongoose.model('Address')
+  , stringUtils = require('../../utils/string-utils.js')
+  , _ = require('lodash')
+;
+
+//UTILITY
+// function unwrapAddress(results) {
+//   return _.map(results, function(result){
+//     var _address = result._address;
+//     console.log(_address)
+//     return _.assign(result, _address);
+//   });
+// }
 
 exports.timeline = function (req, res) {
-  if (!req.user){
-    res.render('home', {
-        favorite_blocks: []
-      , title: 'Home'
-    });
-    return;
-  } else
-  Bookmark.getTimeline(req.user._id, function (err, results) {
-    var hypermarks = _.map(results, '_address');
-    res.render('results', {
-      user: req.user
-      , favorite_blocks: (req.user) ? req.user.getFavoriteBlocks() : null
-      , results: hypermarks
-      , title: 'Timeline'
-      , timeline: true
-    });
-  });
+  if (!req.user) return res.end('401');
+
+  Bookmark.getTimeline(req.user._id, function (err, hypermarks) {
+    res.render('results', {
+        user: req.user
+      , favorite_blocks: req.user.getFavoriteBlocks()
+      , results: hypermarks
+      , title: 'Timeline'
+      , page: 'timeline'
+    });
+  });
 };
 
 exports.publicBlock = function (req, res) {
-  var block = stringUtils.sanitize(req.params.block);
-  Bookmark.getPublicBlock(block, function (err, hypermarks) {
-    res.render('results', {
-      user: req.user
-      , favorite_blocks: (req.user) ? req.user.getFavoriteBlocks() : null
-      , results: hypermarks
-      , title: block
-      , visibility: 'public'
-    });
-  });
+  var block = stringUtils.sanitize(req.params.block);
+  Bookmark.getPublicBlock(block, function (err, hypermarks) {
+    res.render('results', {
+        user: req.user
+      , favorite_blocks: req.user.getFavoriteBlocks()
+      , results: hypermarks
+      , title: block
+      , visibility: 'public'
+      , page: 'block'
+    });
+  });
 };
 
 exports.privateBlock = function (req, res) {
-  var block = stringUtils.sanitize(req.params.block);
-  Bookmark.getPrivateBlock(req.user._id, block, function (err, hypermarks) {
-    res.render('results', {
-        user: req.user
-      , favorite_blocks: req.user.getFavoriteBlocks()
-      , results: hypermarks
-      , title: block
-      , visibility: 'private'
-    });
-  });
+  var block = stringUtils.sanitize(req.params.block);
+  Bookmark.getPrivateBlock(req.user._id, block, function (err, hypermarks) {
+    res.render('results', {
+        user: req.user
+      , favorite_blocks: req.user.getFavoriteBlocks()
+      , results: hypermarks
+      , title: block
+      , visibility: 'private'
+      , page: 'block'
+    });
+  });
 };
 
 exports.search = function (req, res) {
-  Address.search({
-    query: req.query.q
-  }, function (err, results) {
-    var hypermarks = _.map(results.hits, '_source')
-    res.render('results', {
-        user: req.user
-      , favorite_blocks: req.user.getFavoriteBlocks()
-      , results: hypermarks
-      , searchVal: req.query.q
-      , title: 'Search'
-    });
-  });
+  Address.search({
+    query: req.query.q
+  }, function (err, results) {
+    var hypermarks = _.map(results.hits, function(result) {
+      result._address = result._source;
+      delete result._source;
+      return result
+    });
+    res.render('results', {
+        user: req.user
+      , favorite_blocks: req.user.getFavoriteBlocks()
+      , results: hypermarks
+      , title: 'Search'
+      , page: 'search'
+    });
+  });
 };
-
-var hypermarksSourceExtractor=function(sourceList){
-  return _.map(sourceList, function(val){ return val._source;});
-}
--- app/frontend/site/areas.js
+++ app/frontend/site/areas.js
@@ -1,123 +1,123 @@
 'use strict';
 
 var $ = require('../vendor/jquery.js')
-  , airwaves = require('airwaves')
-  , presentational = require('./presentational.js')
-  , sidebarTmpl = require('../../views/includes/sidebar.jade')
+  , airwaves = require('airwaves')
+  , presentational = require('./presentational.js')
+  , sidebarTmpl = require('../../views/includes/sidebar.jade')
 ;
 
 //CHANNELS
 var modesChan = new airwaves.Channel()
-  , dataChan = new airwaves.Channel()
+  , dataChan = new airwaves.Channel()
 ;
 
 
 //MIXINS
 function modalWindow($el) {
-  $el.on('click', function(e){
-    e.stopPropagation();
-  });
-  $el.on('click', '.js-close', function(){
-    modesChan.broadcast('exit');
-  });
+  $el.on('click', function(e){
+    e.stopPropagation();
+  });
+  $el.on('click', '.js-close', function(){
+    modesChan.broadcast('exit');
+  });
 }
 
 
 //AREAS
 exports.results = function ($el) {
-  $el.on('click', '.js-add-to-list', function() {
-    var $hypermark = $(this).parents('.hypermark')
-      , bookmark_id = $hypermark.attr('data-_id')
-    ;
-    $(this).addClass('-active');
-    $hypermark.addClass('top');
-    modesChan.broadcast('add-to-list', bookmark_id);
-  });
+  $el.on('click', '.js-add-to-list', function() {
+    var $hypermark = $(this).parents('.hypermark')
+      , bookmark_id = $hypermark.attr('data-_id')
+    ;
+    $(this).addClass('-active');
+    $hypermark.addClass('top');
+    modesChan.broadcast('add-to-list', bookmark_id);
+  });
 
-  //Subscriptions
-  modesChan.subscribe('exit', function() {
-    $el.find('.js-add-to-list').removeClass('-active');
-    $el.find('.hypermark').removeClass('top');
-  });
+  //Subscriptions
+  modesChan.subscribe('exit', function() {
+    $el.find('.js-add-to-list').removeClass('-active');
+    $el.find('.hypermark').removeClass('top');
+  });
 
 };
 
 exports.header = function ($el) {
-  $el.on('keypress', '#searchbox', function(e) {
-    if (e.keyCode === 13) {
-      window.location = '/_search?q=' + $(this).val();
-    }
-  });
+  $el.on('keypress', '#searchbox', function(e) {
+    if (e.keyCode === 13) {
+      window.location = '/_search?q=' + $(this).val();
+    }
+  });
 };
 
 exports.sidebar = function ($el) {
-  $el.on('click', '.js-new-list', function(){
-    modesChan.broadcast('new-list');
-  });
+  $el.on('click', '.js-new-list', function(){
+    modesChan.broadcast('new-list');
+  });
 
-  //Subscriptions
-  modesChan.subscribe('add-to-list', function(bookmark_id){
-    $el.addClass('top');
-    $el.find('.js-fave-lists').addClass('-hoverable');
+  //Subscriptions
+  modesChan.subscribe('add-to-list', function(bookmark_id){
+    $el.addClass('top');
+    $el.find('.js-fave-lists').addClass('-hoverable');
 
-    $el.on('click.temp', '.js-list', function(e) {
-      e.preventDefault();
-      var block_id = $(this).text();
-      $.post('/_api/blocks', { bookmark_id: bookmark_id, block_id: block_id });
-      presentational.flash($(this), '-added', 1000);
-      modesChan.broadcast('exit');
-    });
-  });
+    $el.on('click.temp', '.js-list', function(e) {
+      e.preventDefault();
+      var block_id = $(this).text();
+      $.post('/_api/blocks', { bookmark_id: bookmark_id, block_id: block_id });
+      presentational.flash($(this), '-added', 1000);
+      modesChan.broadcast('exit');
+    });
+  });
 
-  dataChan.subscribe('favorite_lists', function (data) {
-    var el = sidebarTmpl({
-      favorite_blocks: data
-    });
-    $el.html($('*:first', el).unwrap());
-  });
-  
-  modesChan.subscribe('exit', function() {
-    $el.off('.temp');
-    $el.find('.js-fave-lists').removeClass('-hoverable');
-  });
+  dataChan.subscribe('favorite_lists', function (data) {
+    var el = sidebarTmpl({
+      favorite_blocks: data
+    });
+    $el.html($('*:first', el).unwrap());
+  });
+  
+  modesChan.subscribe('exit', function() {
+    $el.off('.temp');
+    $el.find('.js-fave-lists').removeClass('-hoverable');
+  });
 };
 
 
 exports.newListModal = function($el) {
-  modalWindow($el);
+  modalWindow($el);
 
-  $el.on('click', '.js-add-current', function(){
-    var list_name = $('#page-title').text();
-    $('.js-name').val(list_name);
-  });
+  $el.on('click', '.js-add-current', function(){
+    var list_name = $('#page-title').text();
+    $('.js-name').val(list_name);
+  });
 
-  $el.on('click', '.js-submit', function(){
-    var list_name = $('.js-name').val();
-    $.post('/_api/users/favorites', { block_id: list_name }, function(data) {
-      dataChan.broadcast('favorite_lists', data);
-    });
-    modesChan.broadcast('exit');
-  });
+  $el.on('click', '.js-submit', function(){
+    var list_name = $('.js-name').val();
+    $.post('/_api/users/favorites', { block_id: list_name }, function(data) {
+      dataChan.broadcast('favorite_lists', data);
+    });
+    modesChan.broadcast('exit');
+  });
 
-  modesChan.subscribe('new-list', function() {
-    $el.addClass('-active');
-  });
+  modesChan.subscribe('new-list', function() {
+    $el.addClass('-active');
+  });
 
-  modesChan.subscribe('exit', function(){
-    $el.removeClass('-active');
-  });
+  modesChan.subscribe('exit', function(){
+    $el.removeClass('-active');
+  });
 };
 
 
 exports.modalOverlay = function($el) {
-  $el.on('click', function() {
-    modesChan.broadcast('exit');
-  });
-  
-  modesChan.subscribe('new-list, add-to-list', function(){
-    $el.addClass('-active');
-  });
-  modesChan.subscribe('exit', function(){
-    $el.removeClass('-active');
-  });
+  $el.on('click', function() {
+    modesChan.broadcast('exit');
+  });
+  
+  modesChan.subscribe('new-list, add-to-list', function(){
+    $el.addClass('-active');
+  });
+  modesChan.subscribe('exit', function(){
+    $el.removeClass('-active');
+  });
 };
