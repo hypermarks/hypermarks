4337a082ddd3c083ca2f0d6bc361b715275914fb
Jehan <jehan.tremback@gmail.com>
2013-10-07 21:00:34 -0700

    more config

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@
  * Module dependencies.
  */
 
-var path = require('path')
-var rootPath = path.resolve(__dirname + '../..')
+var path = require('path');
+var rootPath = path.resolve(__dirname + '../..');
 
 /**
  * Expose config
@@ -15,7 +15,11 @@ module.exports = {
     root: rootPath
     , db: 'mongodb://localhost/hypermarks_dev'
     , es: 'localhost:9200'
+    , url: 'localhost:1337'
   },
+  // heroku: {
+  //   url
+  // },
   test: {
     root: rootPath
     , db: 'mongodb://localhost/hypermarks_test'
@@ -29,4 +33,4 @@ module.exports = {
     root: rootPath
     , db: process.env.MONGOHQ_URL
   }
-}
+};
