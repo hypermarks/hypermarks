91d2e9d838d21b37d18d0645fceec06fbf8fd500
Jehan <jehan.tremback@gmail.com>
2013-10-09 12:26:59 -0700

    rename env var

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = function() {
       , db: 'mongodb://localhost/hypermarks_dev'
       , es: 'localhost'
       , url: 'http://localhost:' + port
-      , esport: process.env.ESPORT
+      , esport: process.env.ES_PORT
     };
   }
   if (env === 'heroku') {
@@ -23,7 +23,7 @@ module.exports = function() {
       , db: process.env.MONGOLAB_URI
       , es: process.env.BONSAI_URL
       , url: 'http://hypermarks.herokuapp.com'
-      , esport: process.env.ESPORT
+      , esport: process.env.ES_PORT
     };
   }
 };
